1d457a25fd4b78b3a2e6d361cee4073c1aebb232
Mark VanLandingham <mark@laborchart.com>
2017-12-07 09:14:19 -0600

    anaylsis class generating report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 
+require 'net/http'
 require 'active_model'
 require 'active_model/validations'
 require 'active_record'
@@ -9,6 +10,9 @@ require 'virtus'
 require 'support/model_helpers'
 require 'support/config_helper'
 require 'support/tag_matcher'
+
+require 'validates_accessibility/warning_standards/h69.rb'
+require 'validates_accessibility/warning_standards/h71.rb'
 
 LOCALE_PATH = File.expand_path(File.dirname(__FILE__) + '/../lib/generators/validates_accessibility/templates/en.yml')
 I18n.load_path.unshift(LOCALE_PATH)
@@ -33,4 +37,78 @@ RSpec.configure do |c|
   c.filter_run_excluding :active_record => lambda {|version|
     !(::ActiveRecord::VERSION::STRING.to_s =~ /^#{version.to_s}/)
   }
+end
+
+class Analysis
+  attr_accessor :source
+  attr_accessor :errors
+  attr_accessor :warnings
+  attr_accessor :errorsReported
+  attr_accessor :warningsReported
+
+  def initialize(source, options)
+    @@source = source
+    @@errors = []
+    @@warnings = []
+    @@errorsReported = false
+    @@warningsReported = false
+    @@this = self
+  end
+
+  def generateReport
+    Prawn::Document.generate("report.pdf") do
+      text "ERRORS", :align => :center
+      @@errors.each do |error|
+        text error
+      end
+
+      text "\nWARNINGS", :align => :center
+      @@warnings.each do |warning|
+        text warning
+      end
+    end
+  end
+
+  def runWarningTests
+    RSpec.describe WarningValidator::Warner, "Plugin" do
+      before do
+        Document.show_warnings_of :body
+      end
+      after do 
+        @@warningsReported = true
+        if @@errorsReported == true
+          @@this.generateReport
+        end
+      end
+      it "Testing for accessiblity concerns "+ ENV['URL']+ENV['TEMPLATE'] do
+        source = Net::HTTP.get(ENV['URL'], ENV['TEMPLATE']).to_s
+        c = Document.new
+        c.body = source
+        expect(c).to be_invalid
+        @@warnings = c.errors.messages[:body]
+      end
+    end
+  end
+
+  def runErrorTests
+    RSpec.describe ValidatesAccessibility::Validator, "Plugin" do
+      before do
+        Document.validates_accessibility_of :body
+      end
+      after do 
+        @@errorsReported = true
+        if @@warningsReported == true
+          @@this.generateReport
+        end
+      end
+      it "Testing accessiblity failures of "+ ENV['URL']+ENV['TEMPLATE'] do
+        source = Net::HTTP.get(ENV['URL'], ENV['TEMPLATE']).to_s
+        c = Document.new
+        c.body = source
+        expect(c).to be_invalid
+        @@errors = c.errors.messages[:body]
+      end
+    end
+  end
+
 end
--- spec/validates_accessibility/plugin/plugin_spec.rb
+++ spec/validates_accessibility/plugin/plugin_spec.rb
@@ -2,57 +2,6 @@ require 'net/http'
 require 'nokogiri'
 require "prawn"
 
-# Run all validation tests
-def runErrorTests
-  RSpec.describe ValidatesAccessibility::Validator, "Plugin" do
-    before do
-      Document.validates_accessibility_of :body
-    end
-    after do
-      runWarningTests(@errors)
-    end
-    it "Testing accessiblity failures of "+ ENV['URL']+ENV['TEMPLATE'] do
-      source = Net::HTTP.get(ENV['URL'], ENV['TEMPLATE']).to_s
-      c = Document.new
-      c.body = source
-      expect(c).to be_invalid
-      @errors = c.errors.messages[:body]
-    end
-  end
-end
-
-# Run all warning tests
-def runWarningTests(errors)
-  RSpec.describe WarningValidator::Warner, "Plugin" do
-    before do
-      Document.show_warnings_of :body
-    end
-    after do
-      generatePDF(errors, @warnings)
-    end
-    it "Testing for accessiblity concerns "+ ENV['URL']+ENV['TEMPLATE'] do
-      source = Net::HTTP.get(ENV['URL'], ENV['TEMPLATE']).to_s
-      c = Document.new
-      c.body = source
-      expect(c).to be_invalid
-      @warnings = c.errors.messages[:body]
-    end
-  end
-end
-
-def generatePDF(errors, warnings)
-  Prawn::Document.generate("report.pdf") do
-    text "ERRORS", :align => :center
-    errors.each do |error|
-      text error
-    end
-
-    text "\nWARNINGS", :align => :center
-    warnings.each do |warning|
-      text warning
-    end
-  end
-end
-
-runErrorTests
-
+analysis = Analysis.new(Net::HTTP.get(ENV['URL'], ENV['TEMPLATE']).to_s, nil)
+analysis.runErrorTests
+analysis.runWarningTests
